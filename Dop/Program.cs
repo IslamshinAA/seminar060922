23c8db3d33ff70e54e6d6aa52ffb33787cdb1c39
A.Islamshin <arshavin0511@gmail.com>
2022-09-08 11:53:41 +0300

    dop-

---
+++ Dop/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.WriteLine("Введите интересующий вас количество дней: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Temper(int[] temperature)
+{
+    int lenght = temperature.Length;
+    int i = 0;
+    while(i < lenght)
+    {
+        temperature[i] = new Random().Next(-50,50);
+        i++;
+    }
+    
+}
+int[] array = new int [100];
+Temper(array);
+int i = 0;
+int day = 0;
+if(i < n)
+{
+
+}
+
+    
+    }
+
+
+// int positiveCount=array.Count(x=>x>0);
+// Console.WriteLine();
+// Console.WriteLine(positiveCount + "     ");
